37b740ae2125728366566bbc7af26cbe294649e8
RODRIGO CABEZAS QUIROS <rcabezqu7.alumnes@ub052229.mat.ub.edu>
2020-02-26 19:56:56 +0100

    Client implemented

--- Client/src/main/java/Client.java
+++ Client/src/main/java/Client.java
@@ -1,21 +1,29 @@
-import utils.ComUtilsService;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 public class Client {
 
+    // Has to have -h parameter.
+    // Client parameters -s [SERVER] -p [PORT] -i [0 o 1]
     public static void main(String[] args) {
-        File file = new File("test");
-        try {
-            file.createNewFile();
-            ComUtilsService comUtilsService = new ComUtilsService(new FileInputStream(file), new FileOutputStream(file));
-            comUtilsService.writeTest();
-            System.out.println(comUtilsService.readTest());
-        } catch (IOException e) {
-            System.out.println("Error Found during Operation:" + e.getMessage());
-            e.printStackTrace();
+
+        String serverAdress;
+        int serverPort, gameMode;
+
+        if (args.length == 6 && args[0].equals("-s") && args[2].equals("-p") && args[4].equals("-i")){
+
+            serverAdress = args[1];
+            serverPort = Integer.parseInt(args[3]);
+            gameMode = Integer.parseInt(args[5]);
+
+            if (gameMode < 0 || gameMode > 2){
+                System.out.println("Invalid gamemode. Avaiable options are 0,1 or 2.");
+                System.exit(1);
+            }
+
+        }else if (args.length == 1 && args[0].equals("-h")){
+            System.out.println("Usage: java Client -s <server> -p <port> [-i 0|1|2]");
+        }else{
+            System.out.println("Invalid parameters. Client parameters are: -s <server> -p <port> [-i 0|1|2]");
+            System.exit(1);
         }
 
     }
