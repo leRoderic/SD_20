e08e99d24b37e7317cceb0addeac8815ea866f80
Rodrigo Cabezas Quirós <rcabezasquiros@gmail.com>
2020-05-02 00:00:52 +0200

    cart ui improved // backend reworked

--- P2/front-end/src/components/Events.vue
+++ P2/front-end/src/components/Events.vue
@@ -1,37 +1,43 @@
 <style>
   @import '../assets/button.css';
   @import '../assets/animate.css';
+
   .flex-container {
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
   }
+
   ::-webkit-scrollbar {
     width: 12px;
     background-color: transparent;
   }
+
   ::-webkit-scrollbar-track {
-    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
     border-radius: 10px;
     background-color: transparent;
 
   }
+
   ::-webkit-scrollbar-thumb {
     border-radius: 10px;
     background-color: white;
-    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.5);
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5);
   }
+
   .card-img-top {
     width: 100%;
     height: 15vw;
     object-fit: cover;
   }
+
   ul {
     width: auto;
     columns: 2;
     -webkit-columns: 2;
     -moz-columns: 2;
-}
+  }
 
   #vbgnd {
     min-width: 100%;
@@ -45,65 +51,56 @@
     z-index: -100;
     background-size: cover;
   }
+
   @font-face {
     font-family: proxima;
     src: url('../assets/logo-font.otf') format('opentype');
   }
-  table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-  }
-  td, th {
-    border: 1px solid #dddddd;
-    text-align: left;
-    padding: 8px;
-  }
-  tr {
-    background-color: #dddddd;
-  }
+
 </style>
 <template>
   <div id="app">
     <video autoplay muted loop id="vbgnd">
-      <source src="../assets/video.mp4" type="video/mp4">
+      <source src="../assets/video.mp4" type="video/mp4" id="vbgnd2">
     </video>
-    <div>
-      <nav class="navbar navbar-expand-md navbar-dark bg-dark animated slideInDown" style="margin-top: -60px; margin-bottom: 40px">
-        <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
-        </div>
-        <div class="mx-auto order-0">
-          <a class="navbar-brand mx-auto animated bounceInLeft" href="#" style="font-family: Proxima; font-size: 3rem; margin-top: -10px;
-          margin-bottom: -20px; letter-spacing: 4px; animation-delay: 0.5s">TicketIt!<span class="badge badge-info" style="font-family: Consolas;
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark animated slideInDown"
+         style="margin-top: -60px; margin-bottom: 40px">
+      <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+      </div>
+      <div class="mx-auto order-0">
+        <a class="navbar-brand mx-auto animated bounceInLeft" href="#" style="font-family: Proxima; font-size: 3rem; margin-top: -10px;
+          margin-bottom: -20px; letter-spacing: 4px; animation-delay: 0.5s">TicketIt!<span class="badge badge-info"
+                                                                                           style="font-family: Consolas;
             font-size: 10px">Alpha</span></a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-        </div>
-        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
-          <ul class="navbar-nav ml-auto animated bounceInLeft" style="animation-delay: 0.5s">
-            <li class="nav-item">
-              <div class="button" id="button-2" style="margin: 0px; margin-left: 10px">
-                <div id="slide"></div>
-                <a href="#" onclick="console.info(events_bought)" id="btLogin">View cart</a>
-              </div>
-            </li>
-            <li class="nav-item">
-              <div class="button" id="button-2" style="margin: 0px; margin-left: 10px">
-                <div id="slide"></div>
-                <a href="#" id="btLogin">Login</a>
-              </div>
-            </li>
-            <li class="nav-item">
-              <div class="button" id="button-2" style="margin: 0px; margin-left: 10px">
-                <div id="slide"></div>
-                <a href="#" id="btLogin">Logout</a>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </nav>
-      <div class="flex-container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+        <ul class="navbar-nav ml-auto animated bounceInLeft" style="animation-delay: 0.5s">
+          <li class="nav-item">
+            <div class="button" id="button-2" style="margin: 0px; margin-left: 10px">
+              <div id="slide"></div>
+              <a href="#" @click="this.toggleCart" id="btToggleCart">View cart</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <div class="button" id="button-2" style="margin: 0px; margin-left: 10px">
+              <div id="slide"></div>
+              <a href="#" id="btLogin">Login</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <div class="button" id="button-2" style="margin: 0px; margin-left: 10px">
+              <div id="slide"></div>
+              <a href="#" id="btLogout">Logout</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div id="eventsGrid">
+      <div class="flex-container animated slideInUp">
         <div class="card" style="width: 18rem; opacity: 0.88" v-for="(event, index) in events" :key="event.id">
           <img class="card-img-top" :src="getImgUrl(index)" alt="asd">
           <div class="card-body h-100" style="margin-bottom: -10rem">
@@ -112,7 +109,8 @@
           <div class="card-body" style="background-color: #236bef; color: #ffffff; height: 300px; overflow: auto;
             margin-top: 1rem">
             <div>
-              <h6 v-for="(artist) in event.event.artists" :key="artist.id" style="list-style-type: none">{{artist.name}}</h6>
+              <h6 v-for="(artist) in event.event.artists" :key="artist.id" style="list-style-type: none">
+                {{artist.name}}</h6>
             </div>
           </div>
           <div class="card-body h-100">
@@ -126,33 +124,40 @@
             <h6>Tickets available: <b>{{event.event.total_available_tickets}}</b></h6>
             <div class="button" id="button-2" style="margin-bottom: 0px">
               <div id="slide"></div>
-              <a v-on:click="addEvent(event)">Add to cart</a>
+              <a @click="addEvent(event.event)">Add to cart</a>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <h5 style="margin-top: 80px; color: white">&copy; Copyright {{this.getYear()}} TicketIt!. All Rights Reserved.</h5>
-    <table class="table">
-      <thead>
-        <tr>
-          <th>Event Name</th>
-          <th>Quantity</th>
-          <th>Price(€)</th>
-          <th>Total</th>
-          <th></th>
+    <div class="animated slideInUp" id="cart" style="display: none; background-color: white; opacity: 0.88; width: 90rem">
+      <table class="table">
+        <tr class="thead-dark">
+          <th scope="col">Event name</th>
+          <th scope="col">Quantity</th>
+          <th scope="col">Price</th>
+          <th scope="col">Total</th>
         </tr>
-        <tr v-for="(event, num) in temp" :key="event.id">
-          <th>{{event.event.name}}</th>
-          <th style="text-align: center">
-            {{num = getQuantity(event.event.name)}} - <button class="btn btn-primary" v-on:click="num+=1" style="background-color: #4CAF50;"> + </button>
-            <button class="btn btn-primary" v-on:click="num-=1" style="background-color: #f44336;"> - </button>
-          <th>{{event.event.price}}</th>
-          <th>{{event.event.price * num }}</th>
-          <th><button class="btn btn-primary" v-on:click="eliminar(event.event.name)" style="background-color: #f44336;"> Delete ticket </button></th>
+        <tr class="thead-light" v-for="index in this.events_bought" :key="index.id">
+          <td><b>{{index.name}}</b></td>
+          <td><button type="button" class="btn btn-danger" style="border-radius: 50%; width: 40px; height: 40px;
+            margin-right: 10px"><b>-</b></button>
+            1
+          <button type="button" class="btn btn-success" style="border-radius: 50%; width: 40px; height: 40px;
+            margin-left: 10px"><b>+</b></button>
+          </td>
+          <td><b>{{index.price}}€</b></td>
+          <td><b style="color: #236bef">PENDING€</b></td>
         </tr>
-      </thead>
-    </table>
+      </table>
+      <div style="text-align: center" id="emptyCartMessage">
+        <h2>Your cart is empty! <a href="#" @click="this.toggleCart" style="color: #236bef"><u>Let's fill it up!</u></a></h2>
+      </div>
+    </div>
+    <div id="footer">
+      <h5 style="margin-top: 80px; color: white">&copy; Copyright {{this.getYear()}} TicketIt!. All Rights
+        Reserved.</h5>
+    </div>
   </div>
 </template>
 <script>
@@ -171,12 +176,30 @@ export default {
       price: 10,
       money: 100,
       events_bought: [],
-      events: [],
-      temp: []
+      events: []
     }
   },
 
   methods: {
+
+    toggleCart () {
+      // eslint-disable-next-line eqeqeq
+      if (document.getElementById('eventsGrid').style.display != 'none') {
+        document.getElementById('eventsGrid').style.display = 'none'
+        document.getElementById('cart').style.display = 'inline-block'
+        document.getElementById('btToggleCart').firstChild.data = 'Back to events'
+      } else {
+        document.getElementById('eventsGrid').style.display = 'inline-block'
+        document.getElementById('cart').style.display = 'none'
+        document.getElementById('btToggleCart').firstChild.data = 'View cart'
+      }
+      // eslint-disable-next-line eqeqeq
+      if (this.events_bought.length == 0) {
+        document.getElementById('emptyCartMessage').style.display = 'block'
+      } else {
+        document.getElementById('emptyCartMessage').style.display = 'none'
+      }
+    },
 
     getYear () {
       return new Date().getFullYear()
@@ -201,10 +224,7 @@ export default {
         })
     },
     addEvent (event) {
-      this.events_bought.push(event)
-      if (!this.temp.includes(event)) {
-        this.temp.push(event)
-      }
+      this.events_bought.push({'name': event.name, 'id': event.id, 'price': event.price})
     },
     getQuantity (name) {
       var q = 0
