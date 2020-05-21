8b9fdfa04a97250c8a7d035fe617e5426560e8b5
Rodrigo Cabezas Quirós <rcabezasquiros@gmail.com>
2020-05-21 13:46:37 +0200

    ASDKH

--- db.py
+++ db.py
@@ -10,7 +10,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     environment = config['development']
-    if config_decouple('PRODUCTION'):
+    if config('PRODUCTION', default='localhost'):#config_decouple('PRODUCTION'):
         environment = config['production']
     app.config.from_object(environment)
     db = SQLAlchemy(app)
