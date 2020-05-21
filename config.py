5c13bb0a8eff95038248cc468c9b92091c187aac
Rodrigo Cabezas Quirós <rcabezasquiros@gmail.com>
2020-05-21 14:05:17 +0200

    ASDJ2

--- config.py
+++ config.py
@@ -7,16 +7,16 @@ class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = config('DATABASE_URL', default='localhost')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     STATIC_FOLDER = "/static"
-    TEMPLATE_FOLDER = "/templates"
+    TEMPLATE_FOLDER = "/templatess"
     SECRET_KEY = config('SECRET_KEY', default='localhost')
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = config('DATABASE_URL', default='localhost')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    STATIC_FOLDER = "/static"
-    TEMPLATE_FOLDER = "/templatess"
-    SECRET_KEY = config('SECRET_KEY', default='localhost')
+    STATIC_FOLDER = "../P2/front-end/dist/static"
+    TEMPLATE_FOLDER = "../P2/front-end/dist"
+    SECRET_KEY = "M2CjZ52aDgtvt3XXRWBktEPY64EqtxhJ"
 
 config = {
     'development': DevelopmentConfig,
