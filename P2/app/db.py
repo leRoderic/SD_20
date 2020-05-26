0af07457a6148a04dd186a04b80171291f70950e
Rodrigo Cabezas Quirós <rcabezasquiros@gmail.com>
2020-05-26 19:52:24 +0200

    ASD

--- P2/app/db.py
+++ P2/app/db.py
@@ -7,13 +7,10 @@ secret_key = "M2CjZ52aDgtvt3XXRWBktEPY64EqtxhJ"
 
 
 def create_app():
-    app = Flask(__name__,
-                static_folder="../front-end/dist/static",
-                template_folder="../front-end/dist")
-    app.config.from_object(__name__)
-    CORS(app, resources={r'/*': {'origins': '*'}})
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = secret_key
+    app = Flask(__name__)
+    environment = config['development']
+    if config_decouple('PRODUCTION', cast=bool, default=False):
+        environment = config['production']
+    app.config.from_object(environment)
     db = SQLAlchemy(app)
     return app
